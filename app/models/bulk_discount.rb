77d221ca5778aaf678ec10fd4614503125c755c6
Robert Heath <robert.b.heathii@gmail.com>
2021-01-18 17:30:12 -0600

    Update validations for numericality in bulk discount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -4,4 +4,7 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage_off
   validates_presence_of :quantity_threshold
   validates_presence_of :merchant
+
+  validates :percentage_off, numericality: { greater_than: 0, less_than: 1 }
+  validates :quantity_threshold, numericality: { greater_than: 0 }
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_presence_of :merchant}
     it { should validate_presence_of :quantity_threshold }
     it { should validate_presence_of :percentage_off }
-    it { should validate_numericality_of(:percentage_off), only_float: true }
     it { should validate_numericality_of(:percentage_off).is_greater_than(0) }
     it { should validate_numericality_of(:percentage_off).is_less_than(1) }
     it {should validate_numericality_of(:quantity_threshold).is_greater_than(0)}
