975b1855f0fdceaea3ac7615564f925789083171
Robert Heath <robert.b.heathii@gmail.com>
2021-01-18 17:33:55 -0600

    Complete creation of new page for bulk discounts

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -4,7 +4,23 @@ class Merchant::BulkDiscountsController < ApplicationController
   end
 
   def new
-
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.create(discount_params)
+    if @discount.id
+      redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
+    else
+      flash[:notice] = @discount.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:merchant_id, :quantity_threshold, :percentage_off)
+  end
 end
--- spec/features/merchant/bulk_discounts/new_spec.rb
+++ spec/features/merchant/bulk_discounts/new_spec.rb
@@ -8,33 +8,32 @@ RSpec.describe 'bulk discounts new page' do
     @bulk_discount2 = BulkDiscount.create!(quantity_threshold: 8, merchant_id: @merchant1.id, percentage_off: 0.15)
     @bulk_discount3 = BulkDiscount.create!(quantity_threshold: 10, merchant_id: @merchant1.id, percentage_off: 0.25)
 
-    visit new_merchant_bulk_discount_path
+    visit new_merchant_bulk_discount_path(@merchant1)
   end
 
   describe 'when I visit the bulk discounts page' do
     it 'has a form for a new bulk discount' do
       expect(page).to have_field(:quantity_threshold)
       expect(page).to have_field(:percentage_off)
-      expect(page).to have_hidden_field(:merchant_id)
+      page.has_field? :merchant_id, type: :hidden, with: @merchant1.id
     end
 
     it 'shows an error if any of the fields are empty' do
+      fill_in :quantity_threshold, with: 10
+      click_on "Create Discount"
 
-      click_button "Create Discount"
-
-      expect(page).to have_content("Quantity Threshold must not be empty.")
-      expect(page).to have_content("Percentage off must not be empty.")
+      expect(page).to have_content("Percentage off can't be blank")
     end
 
     it 'shows an error if any of the inputs are invalid' do
 
       fill_in :quantity_threshold, with: 'five'
-      fill_in :percentage_off, with: 'quantity'
+      fill_in :percentage_off, with: 1.25
 
-      click_button "Create Discount"
+      click_on "Create Discount"
 
-      expect(page).to have_content("Quantity Threshold must not be empty.")
-      expect(page).to have_content("Percentage off must not be empty.")
+      expect(page).to have_content("Percentage off must be less than 1")
+      expect(page).to have_content("Quantity threshold is not a number")
     end
 
     it 'redirects me back to the index page after successful creation of a bulk discount' do
@@ -42,9 +41,9 @@ RSpec.describe 'bulk discounts new page' do
       fill_in :quantity_threshold, with: 25
       fill_in :percentage_off, with: 0.23
 
-      click_button "Create Discount"
+      click_on "Create Discount"
 
-      expect(current_path).to eq(merchant_bulk_discounts_path)
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
     end
   end
 end
