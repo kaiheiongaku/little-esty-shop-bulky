863763a8dfaf004c32f8e604c09f44c580eb05b1
Robert Heath <robert.b.heathii@gmail.com>
2021-01-19 17:32:07 -0600

    Write test in invoice item for finding dmax discount for an invoice item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -12,4 +12,36 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :invoice }
     it { should belong_to :item }
   end
+
+  describe 'instance methods' do
+    describe "finding the highest discount" do
+      before :each do
+        @merchant1 = Merchant.create!(name: 'Hair Care')
+        @merchant2 = Merchant.create!(name: 'Classic Instruments')
+
+        @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @invoice_1 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 5, unit_price: 10, status: 2)
+        @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 10, unit_price: 10, status: 1)
+
+        @bd1 = BulkDiscount.create!(merchant_id: @merchant1.id, quantity_threshold: 5, percentage_off: 0.1)
+
+        @bd2 = BulkDiscount.create!(merchant_id: @merchant1.id, quantity_threshold: 10, percentage_off: 0.2)
+
+        @bd3 = BulkDiscount.create!(merchant_id: @merchant2.id, quantity_threshold: 10, percentage_off: 0.15)
+      end
+
+      it 'invoice_item quantity is the lowest tier discount' do
+
+        expect(@ii_1.maximum_discount).to eq(0.1)
+      end
+
+      it 'invoice_item meets threshold for second tier' do
+
+        expect(@ii_11.maximum_discount).to eq(0.2)
+      end
+    end
+  end
 end
