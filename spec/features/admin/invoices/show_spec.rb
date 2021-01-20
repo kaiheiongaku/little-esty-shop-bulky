c85561556cef28216579e88eedc767bb8356d184
Robert Heath <robert.b.heathii@gmail.com>
2021-01-19 22:30:07 -0600

    Create test and input total discounted revenue onto admin invoice page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -12,7 +12,7 @@ describe 'Admin Invoices Index Page' do
 
     @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
     @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
-   
+
     @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
     @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
@@ -69,5 +69,9 @@ describe 'Admin Invoices Index Page' do
       expect(@i1.status).to eq('complete')
     end
   end
-end
 
+  it 'shows total revenue with discounts included' do
+
+    expect(page).to have_content("Total Discounted Revenue: $#{@i1.discounted_total_revenue}")
+  end
+end
