b53033550037a85a436ea45cafefb8805caa620c
Robert Heath <robert.b.heathii@gmail.com>
2021-01-19 21:11:56 -0600

    Add find by percent method in bulk discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -7,4 +7,8 @@ class BulkDiscount < ApplicationRecord
 
   validates :percentage_off, numericality: { greater_than: 0, less_than: 1 }
   validates :quantity_threshold, numericality: { greater_than: 0 }
+
+  def self.find_by_percent(merchant_id, percent)
+    BulkDiscount.where(merchant_id: merchant_id).where(percentage_off: percent).first
+  end
 end
